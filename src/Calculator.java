e8b954ca7ac751896dbbe42c7c275b467c2b2f39
Mecid elhasan <halabylilabad2@gmail.com>
2023-02-25 23:14:27 +0300

    added brackets precedence and the '-' and '+' signs before the operands, are handled in the operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,12 @@
+import java.util.Stack;
+
 public class Calculator {
-    String expression ;
+    String exp ;
     Double previousNum ;
     Double nextNum;
     boolean isNegative = false;
 
-    boolean isNumber(char c){
+    boolean isDigit(char c){
         return switch (c) {
             case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> true;
             default -> false;
@@ -16,85 +18,178 @@ public class Calculator {
             default -> false;
             };
     }
-    String Calculate(String expression){
-        this.expression = expression;
-        while (this.expression.contains("*") || this.expression.contains("/") || this.expression.contains("+")){
-            // 1. parentheses
-            // 2. powers and roots
-            this.expression = multiplyOrDivide(expression); // 3.
-            // 4. addition and subtract
+    String Calculate(String exp){
+        this.exp = exp;
+        checkParenthesesSets(this.exp);
+        // 1. parentheses
+        while (this.exp.contains("(")){
+            this.exp = parentheses(this.exp);
         }
-        return this.expression;
+        // 2. powers and roots
+        this.exp = multiplyOrDivide(this.exp);
+        this.exp = addAndSubtract(this.exp);
+        return this.exp;
     }
-    String multiplyOrDivide(String expression){
-        if (expression.contains("*") || expression.contains("/")){
-            int i = expression.indexOf('*');
-            int j = expression.indexOf("/");
-            if (i!=-1 && (i<j || j==-1 )){
-                if (i ==0)
-                    throw new UnKnownSignException("you can not type '*' at the beginning of the expression !");
-                if(isSign(expression.charAt(i-1)))
-                    throw new UnKnownSignException("you can not type '"+ expression.charAt(i-1) +"' directly before '*' !");
-                multiplyOrDivide('*',i);
-            }else if (j == 0) throw new UnKnownSignException("you can not type '/' at the beginning of the expression !");
-            else if (j>0) {
-                if(isSign(expression.charAt(j-1)))
-                    throw new UnKnownSignException("you can not type '"+ expression.charAt(j-1) +"' directly before '/' !");
-                multiplyOrDivide('/',j);
+    void checkParenthesesSets(String expression){
+        Stack<Character> parentheses = new Stack<>();
+        for (int i=0;i<expression.length();i++){
+            if (expression.charAt(i)=='(' || expression.charAt(i)==')'){
+                if (i==0 && expression.charAt(i)==')')
+                    throw new UnKnownSignException("you can't type ')' at the beginning of the expression !");
+                else if (parentheses.empty() || expression.charAt(i) == parentheses.peek())
+                    parentheses.push(expression.charAt(i));
+                else if(expression.charAt(i) != parentheses.peek()){
+                    parentheses.pop();
+                }
             }
         }
-        if (!this.expression.contains("*") && !this.expression.contains("/"))
-            return this.expression;
-        return multiplyOrDivide(this.expression);
     }
-    void multiplyOrDivide(char sign ,int indicator){
-        int p;
-        for (p = indicator-1;p>0;p--){
-            if (!isNumber(expression.charAt(p)) && expression.charAt(p) != '.')
-                break;
+    String parentheses(String exp){
+        if (exp.contains("(")) {
+            int i = exp.indexOf('(');
+            int j = i+1;
+            int c = 1;
+            for (;;j++){
+                if (exp.charAt(j)=='(') c++;
+                else if (exp.charAt(j)==')') {
+                    c--;
+                    if (c==0)
+                        break;
+                }
+            }
+            boolean IsMultiplied = false;
+            String innerExp = exp.substring(i+1, j);
+            StringBuilder sb = new StringBuilder(exp);
+            if (i>0 && isDigit(exp.charAt(i-1))) {
+                IsMultiplied = true;
+            }else if ( i>0 && (exp.charAt(i-1)=='-' || exp.charAt(i-1)=='+')){
+                int x;
+                for (x = i-1;x>=0;x--){
+                    if (isDigit(exp.charAt(x)))
+                        break;
+                    if (exp.charAt(x)=='-') {
+                        isNegative = !isNegative;
+                        sb.deleteCharAt(x);
+                    }
+                    else if (exp.charAt(x)=='+')
+                        sb.deleteCharAt(x);
+                }
+                if (isNegative) {
+                    sb.insert(i-1,'-');
+                }else{
+                    sb.insert(i-1,'+');
+                }
+            }
+            isNegative = false;
+            String result = parentheses(innerExp);
+            sb.delete(i,innerExp.length()+i+2);
+            sb.insert(i,result);
+            if (IsMultiplied) sb.insert(i,'*');
+            exp = sb.toString();
+            return exp;
         }
+        // powers and roots
+        exp = multiplyOrDivide(exp);
+        exp = addAndSubtract(exp);
+        return exp;
+    }
+    String multiplyOrDivide(String exp){
+        char multiply = '*';char divide = '/';
+        exp = exceptionAssistant(multiply,divide,exp);
+        if (!exp.contains("*") && !exp.contains("/"))
+            return exp;
+        return multiplyOrDivide(exp);
+
+    }
+    boolean hasTwoNumber(String exp){
+        boolean passedFirstNum = false;
+        boolean passedFirstSign = false;
+        for (int i = 0;i<exp.length();i++){
+            if (isDigit(exp.charAt(i)) || exp.charAt(i)=='.'){
+                passedFirstNum = true;
+                if (passedFirstSign) return true;
+            }
+            else if (isSign(exp.charAt(i)) && passedFirstNum)
+                passedFirstSign = true;
+        }
+        return false;
+    }
+    String addAndSubtract(String exp){
+        char plus = '+';char minus='-';
+        exp = exceptionAssistant(plus,minus,exp);
+        // if length of exp is equal to 1 ,no need to check for '-' sign after the first index , anyway we can't because of the substring method working mechanism
+        if (!hasTwoNumber(exp))
+            return exp;
+        return addAndSubtract(exp);
+    }
+    String exceptionAssistant(char a,char b,String exp){
+        if((a=='*' || a =='/') && exp.charAt(0)==a)
+            throw new UnKnownSignException("you can not type '"+exp.charAt(0)+"' at the beginning of the expression !");
+        if (exp.contains(a+"") || exp.contains(b+"")) {
+            int i;
+            boolean passedDigit =false;
+            for (i = 0; i < exp.length(); i++) {
+                if ((exp.charAt(i)==a || exp.charAt(i)==b) && passedDigit)
+                    break;
+                if(isDigit(exp.charAt(i)))
+                    passedDigit = true;
+            }
+            if(i<exp.length()) {
+                if (( exp.charAt(i) == '-' || exp.charAt(i) == '+') &&( exp.charAt(i + 1) == '*' || exp.charAt(i + 1) == '/' || exp.charAt(i + 1) == '^'))
+                    throw new UnKnownSignException("you can not type '" + exp.charAt(i) + "' directly before '" + exp.charAt(i + 1) + "' !");
+                exp = assistant(exp,i);
+            }
+        }
+        return exp;
+    }
+    String assistant(String exp,int signIndicator){
+        char sign = exp.charAt(signIndicator);
+        int p =signIndicator;
+        while (p!=0 && (isDigit(exp.charAt(p-1)) || exp.charAt(p-1) == '.')){
+            p--;
+        }
+
         StringBuilder num= new StringBuilder();
-        for (int n=p;n<expression.length();n++){
-            if (isNumber(expression.charAt(n) ) || expression.charAt(n) == '.')
-                num.append(expression.charAt(n));
-            else if(n==p) continue;
-            else break;
+        for (int n=p;n<exp.length() && (isDigit(exp.charAt(n) ) || exp.charAt(n) == '.');n++){
+                num.append(exp.charAt(n));
         }
-        if (num.toString().contains(".".repeat(2))) throw new RuntimeException("the number should not have two dots in it !");
+        if (!(num.toString().contains(".".repeat(0)) || num.toString().contains(".".repeat(1)))) throw new RuntimeException("the number should not have two dots in it !");
         previousNum = Double.parseDouble(num.toString());
-        while (true) {
-            if (p==-1) {p++;break;}
-            if (expression.charAt(p) !='-') break;
-            else {isNegative = !isNegative;p--;}
+        while (p!=0 && (exp.charAt(p-1) =='-' || exp.charAt(p-1) =='+') ) {
+            if(exp.charAt(p-1) =='-' )
+                isNegative = !isNegative;
+            p--;
         }
         int lowerIndex = p;
         if (isNegative) previousNum = -previousNum;
         // return isNegative to false for using it later.
         isNegative = false;
         //  until here we got the number before the * or / sign and assigned the negativity if exists .
-        int n ;
-        for (n =indicator+1 ;n<expression.length();n++){
-            if (expression.charAt(n)=='-')
+        int n;
+        for (n =signIndicator+1 ;n<exp.length() && (exp.charAt(n) =='-' || exp.charAt(n) =='+');n++){
+            if(exp.charAt(n) =='-' )
                 isNegative = !isNegative;
-            else break;
         }
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
         int ii;
-        for ( ii = n;ii<expression.length();ii++){
-            if (!isNumber(expression.charAt(ii)) && expression.charAt(ii) != '.')
-                break;
-            else stringBuilder.append(expression.charAt(ii));
+        for ( ii = n;ii<exp.length() && (isDigit(exp.charAt(ii)) || exp.charAt(ii) == '.');ii++){
+             num2.append(exp.charAt(ii));
         }
-        nextNum = Double.parseDouble(stringBuilder.toString());
-        StringBuilder sb = new StringBuilder(expression);
+        nextNum = Double.parseDouble(num2.toString());
+        StringBuilder sb = new StringBuilder(exp);
         sb.delete(lowerIndex,ii);
-        String calculated="";
-        if (sign =='*')
-            calculated = (previousNum*nextNum)+"";
-        else if (sign =='/')
-            calculated = (previousNum/nextNum)+"";
-
-        sb.insert(lowerIndex,calculated);
-        expression = sb.toString();
+        String calculated;
+        double D_Calculated=0;
+        switch (sign){
+            case '*' -> D_Calculated = (previousNum*nextNum);
+            case '/' -> D_Calculated = (previousNum/nextNum);
+            case '+' -> D_Calculated = (previousNum+nextNum);
+            case '-' -> D_Calculated = (previousNum-nextNum);
+        }
+        calculated = D_Calculated+"";
+        String signToBeInserted = D_Calculated<0 && (exp.charAt(lowerIndex)=='+')?"":"+";
+        sb.insert(lowerIndex,signToBeInserted + calculated);
+        exp = sb.toString();
+        return exp;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner =new Scanner(System.in);
         Calculator calculator= new Calculator();
         System.out.print("Enter an arithmetic expression : ");
-        String input = "---0.1*25/2*2";
-        System.out.println(input);
+        String input = "((5(1))---0.1*12.5*2-2)";//scanner.nextLine();// "---0.1*25/2*2-2";  // +(5---0.1*(25/2)*2-2)  //((5)---0.1*12.5*2-2)
+        //System.out.println(input);
         String s = calculator.Calculate(input);
         System.out.println(s);
     }
